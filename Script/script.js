30c4c2a26f98122f5a2797fc6a0370dc80947b7b
Astral <132731805+astralsc@users.noreply.github.com>
2025-11-04 09:26:24 +0800

    remove

--- Script/script.js
+++
@@ -1,5 +0,0 @@
-Interceptor.attach(Module.findExportByName("libc.so", "getaddrinfo"), {
-    onEnter(args) {
-        args[0].writeUtf8String("127.0.0.1");
-    }
-});
---
+++ start.bat
@@ -0,0 +1,5 @@
+TITLE CoC - 5.2.4
+cd CoC
+pip install -r requirements.txt
+py Main.py
+pause
